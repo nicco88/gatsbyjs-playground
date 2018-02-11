81cb5cff9d13e5ab649f5bedce26cceaf399bb6b
Niccolò <perego.nicco@gmail.com>
2018-02-11 23:44:33 +0800

    start test project

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-postcss-sass`,
+      options: {
+        postCssPlugins: [
+          require('postcss-import')(),
+          require('postcss-nested')(),
+        ],
+        precision: 8, // SASS default: 5
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    'gatsby-transformer-remark'
+  ]
+};
